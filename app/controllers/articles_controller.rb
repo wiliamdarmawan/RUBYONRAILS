09d29d4f6c038c98067f1c303600492e7f4c0b89
wiliamdarmawan <generousbil@gmail.com>
2022-09-02 15:44:12 +0700

    Save newly created article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,10 +1,18 @@
 class ArticlesController < ApplicationController
   def show
-    binding.break
     @article = Article.find(params[:id])
   end
 
   def index
     @articles = Article.all
+  end
+
+  def new
+  end
+
+  def create
+    @article = Article.new(params.require(:article).permit(:title, :description))
+    @article.save
+    redirect_to @article
   end
 end
