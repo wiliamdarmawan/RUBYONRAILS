ab734cc0e327b79a662a7bf340a1532ddc0debd1
Wiliam Darmawan <Wiliam.Darmawan@unit4.com>
2022-09-16 13:41:25 +0700

    Add users index page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -32,6 +32,10 @@ class UsersController < ApplicationController
     @articles = @user.articles
   end
 
+  def index
+    @users = User.all
+  end
+
   private
   def user_params
     params.require(:user).permit(:username, :email, :password)
