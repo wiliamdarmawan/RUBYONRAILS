959efed0e0c0fc411ea6a95b0501cace6ba42767
Wiliam Darmawan <Wiliam.Darmawan@unit4.com>
2022-09-20 10:22:07 +0700

    Category and model testing

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_19_092529) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_20_031657) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "description"
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer "user_id"
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
