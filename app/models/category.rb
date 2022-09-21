25d10420a5645eed7ad6af619aef6ac72aa4ce5a
Wiliam Darmawan <Wiliam.Darmawan@unit4.com>
2022-09-21 11:29:07 +0700

    Many to many associations - backend

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,6 @@
 class Category < ApplicationRecord
+  has_many :article_category
+  has_many :articles, through: :article_category
   validates :name, presence: true, length: { minimum: 3, maximum: 25 }
   validates_uniqueness_of :name
 end
